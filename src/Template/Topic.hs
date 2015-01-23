68db10c85121274c14183e00b0982c4fe1706cae
Kenni <kenni.bawden@gmail.com>
2015-01-23 14:00:16 +1100

    Added template for displaying a list of topics

--- src/Template/Topic.hs
+++ src/Template/Topic.hs
@@ -2,8 +2,15 @@
 
 module Template.Topic where
 
+import Data.Monoid (mconcat)
+
 import Lucid
 import Model
 
 topics :: [Topic] -> Html ()
-topics ts = p_ "Hello world"
+topics ts = ul_ $ mconcat $ map (li_ . topicSimple) ts
+
+topicSimple :: Topic -> Html ()
+topicSimple topic = do
+    h1_ $ toHtml $ topicTitle topic
+    p_ $ toHtml $ topicSummary topic
