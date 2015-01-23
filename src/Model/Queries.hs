c422a516349aac8d27bc3e772c8772bdeff5d483
Kenni <kenni.bawden@gmail.com>
2015-01-23 13:58:28 +1100

    Fixed topics query to return all topics

--- src/Model/Queries.hs
+++ src/Model/Queries.hs
@@ -2,15 +2,15 @@ module Model.Queries where
 
 import Database.Persist (Entity, insert, get, entityVal)
 import Database.Persist.Sql (SqlPersistT)
-import Database.Esqueleto (select, from, (^.), (?.), (==.), on, InnerJoin(..), just)
+import Database.Esqueleto (select, from, (^.), (?.), (==.), on, InnerJoin(..), LeftOuterJoin(..), just)
 
 import Model
 
 getTopics :: SqlPersistT IO [Topic]
 getTopics = do
-    -- Select all concepts in the DB, and get the topic associated with them.
+    -- Select all topics in the DB, and the concepts associated with them.
     topics <- select $
-        from $ \(concept `InnerJoin` topic) -> do
+        from $ \(topic `LeftOuterJoin` concept) -> do
         on (concept ^. ConceptTopic ==. just (topic ^. TopicId))
         return topic
     -- entityVal upwraps database entities.
