1be9b6dd72e89af2672f0e08aca8bca50c4992aa
Kenni <kenni.bawden@gmail.com>
2015-01-30 11:12:31 +1100

    Fixed form-methods.js browser history behaviour

--- static/js/form-methods.js
+++ static/js/form-methods.js
@@ -6,35 +6,57 @@ var formMethods = function(options) {
         // Create request
         var request = new XMLHttpRequest();
 
+        // Update the [page with completed request...
+        var updatePage = function(options) {
+            // Update page
+            document.body.innerHTML = this.response;
+
+            // Fake history
+            if (options.history)
+                window.history.pushState({
+                    method: form.attributes.method.value,
+                    action: form.action
+                }, form.attributes.method.value, form.action);
+        };
+
         // On form submission...
         var onsubmit = form.onsubmit;
-        form.onsubmit = function(event) {
+        var doRequest = function(event) {
             // Stop the form from submitting
-            event.preventDefault();
+            if (event)
+                event.preventDefault();
             // Do the onsubmit events
             if (onsubmit)
                 onsubmit();
+            // Update the page on request completion 
+            if (options.update) {
+                var update = function() {
+                    // Only replace history if part of onsubmit event
+                    if (event)
+                        updatePage.call(this, {history: true});
+                    // Otherwise just update the page and don't modify the history
+                    else
+                        updatePage.call(this, {history: false});
+                };
+                request.addEventListener("load", update, false);
+                request.addEventListener("error", update, false);
+            }
             // Perform request
             request.open(form.attributes.method.value, form.action);
             request.send(new FormData(form));
         };
 
-        // When the request is done...
-        var loadPage = function() {
-            if (options.update) {
-                // Update page
-                document.body.innerHTML = this.response;
+        form.onsubmit = doRequest;
 
-                // Fake history
-                window.history.pushState(null, form.attributes.method.value, form.action);
-                window.onpopstate = function() {
-                    window.location.reload();
-                };
-            }
+        // Deal with fake history
+        window.onpopstate = function(event) {
+            // If history is a request then replicate the request
+            if (event.state && event.state.method)
+                doRequest();
+            // Otherwise just reload the page
+            else
+                window.location.reload();
         };
-
-        request.addEventListener("load", loadPage, false);
-        request.addEventListener("error", loadPage, false);
     });
 };
 
