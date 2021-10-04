50683129f5b58c584ab2961b51ddb797a231b939
Michael T. Wells <michaelthomaswells@gmail.com>
2021-10-03 23:51:25 -0600

    Update RunDB.hs
    type variable 'm' not in scope.

--- lib/CourseStitch/Models/RunDB.hs
+++ lib/CourseStitch/Models/RunDB.hs
@@ -5,6 +5,6 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Database.Persist.Sql (ConnectionPool, runSqlPool, SqlPersistT)
 import Web.Scotty (ActionM)
 
-type RunDB = MonadIO m => (forall a. SqlPersistT IO a -> m a)
+type RunDB = forall m. MonadIO m => (forall a. SqlPersistT IO a -> m a)
 runDB' :: ConnectionPool -> RunDB
 runDB' pool q = liftIO $ runSqlPool q pool
